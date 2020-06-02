f2045776ee90508dbd06d94474898e759962f5bd
Dan Gelok <dgelok@gmail.com>
2020-06-02 15:18:52 -0500

    ex1 using if and while statements

---
+++ ex1.py
@@ -0,0 +1,30 @@
+
+
+
+
+# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+# if length_of_name > 0:
+#     name_of_friend = input("What is your friend's name? ")
+#     print(greeting % (name_of_user, name_of_friend))
+# else:
+#     print("OK, I'll ask again some other time.")
+
+
+
+
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+
+
+# if length_of_name > 0:
+#     name_of_friend = input("What is your friend's name? ")
+#     greeting = f"Hello {name_of_user}, it is very nice to meet you and your friend {name_of_friend}!"
+#     print(greeting)
+# elif length_of_name > 10:
+#     print("Wow, that's a long name")
+# else:
+#     print("OK, I'll ask again some other time.")
+
+

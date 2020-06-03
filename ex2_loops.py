e7c565d2b6ed2cfb0d5ce9dafe81fe781f46c0fe
Dan Gelok <dgelok@gmail.com>
2020-06-03 17:09:50 -0500

    Loops 1-6

---
+++ ex2_loops.py
@@ -0,0 +1,45 @@
+# I am assuming all user-generated input is within the scope of the question. 
+
+# #1
+# for i in range(1, 11):
+#     print(i)
+
+# #2
+# start = int(input("Which number shall we start on? > "))
+# end = int(input("Which number shall we end on? > "))
+
+# for i in range(start, end+1):
+#     print(i)
+
+# #3
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+
+# ### OR ###
+
+# for i in range(1, 10):
+#     if i % 2 != 0:
+#         print(i)
+
+# #4
+
+# for i in range(5):
+#     print("*" * 5)
+
+# #5
+# num = int(input("How big is the square? > "))
+# for i in range(num):
+#     print("*" * num)
+
+# #6
+# height = int(input("How tall should the box be? > "))
+# width = int(input("How wide should the box be? > "))
+# for i in range(height):
+#     if i == 0 or i == (height - 1):
+#         print("*" * width)
+#     else: 
+#         print("*" + (" " * (width - 2) + "*"))
+
+#7

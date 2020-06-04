95bea51f39b4a736800da7aa28a7614e8964cc7e
Dan Gelok <dgelok@gmail.com>
2020-06-03 19:43:21 -0500

    all lists exercises except bonus

--- ex2_loops.py
+++ ex2_loops.py
@@ -42,4 +42,33 @@
 #     else: 
 #         print("*" + (" " * (width - 2) + "*"))
 
-#7
+
+#7 AND 8
+
+# while True:
+#     height = int(input("How high shall the tree be? > "))
+#     if height < 0:
+#         print("Sorry, we need a positive number.")
+#     if height % 2 == 0:
+#         print("Sorry, we need an odd number")
+#     else:
+#         break
+
+# rows = []
+# i = 0
+# while 2*i < height:
+#     newrow = " "*i + "*"*(height - i*2) + " "*i
+#     rows.append(newrow)
+#     i += 1
+
+# index = len(rows)
+# for x in range(len(rows)-1, 0, -1):
+#     print(rows[x])
+
+#9
+for i in range(1,11):
+    for j in range(1, 11):
+        product = i * j
+        print(f"{i} X {j} = {product} ")
+
+#bonus 

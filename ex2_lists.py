a12f56383d36e4a3a0109b1b974b403fe4235dd2
Dan Gelok <dgelok@gmail.com>
2020-06-04 10:14:07 -0500

    minor changes

--- ex2_lists.py
+++ ex2_lists.py
@@ -2,98 +2,98 @@
 # All outputs are printed for verification.
 
 
-# #1
-# nums = [4,5,1,4,8,3,6,0]
+#1
+nums = [4,5,1,4,8,3,6,0]
 
-# sum = 0
-# for num in nums:
-#     sum += num
-# print(sum)
+sum = 0
+for num in nums:
+    sum += num
+print(sum)
 
-# #2
-# nums = [4,5,100,4,2,3,6,0]
-# big = 0
-# for num in nums:
-#     if big < num:
-#         big = num
-# print(big)
+#2
+nums = [4,5,100,4,2,3,6,0]
+big = 0
+for num in nums:
+    if big < num:
+        big = num
+print(big)
 
-# #3
-# nums = [4,5,100,4,-4,3,6,0]
-# small = 0
-# for num in nums:
-#     if small > num:
-#         small = num
-# print(small)
+#3
+nums = [4,5,100,4,-4,3,6,0]
+small = 0
+for num in nums:
+    if small > num:
+        small = num
+print(small)
 
 #4
-# nums = [4,5,100,4,-4,3,6,0]
-# for num in nums: 
-#     if num % 2 == 0:
-#         print(num)
+nums = [4,5,100,4,-4,3,6,0]
+for num in nums: 
+    if num % 2 == 0:
+        print(num)
 
-# #5
-# nums = [-4,-5,100,4,-4,3,6,0]
-# for num in nums: 
-#     if num > 0:
-#         print(num)
+#5
+nums = [-4,-5,100,4,-4,3,6,0]
+for num in nums: 
+    if num > 0:
+        print(num)
 
 #6
-# nums = [-4,-5,100,4,-4,3,6,0]
-# positives = []
-# for num in nums: 
-#     if num > 0:
-#         positives.append(num)
-# print(positives)
+nums = [-4,-5,100,4,-4,3,6,0]
+positives = []
+for num in nums: 
+    if num > 0:
+        positives.append(num)
+print(positives)
 
 #7
 
-# myFactor = 12
-# nums = [-4,-5,100,4,-4,3,6,0]
-# final = []
-# for num in nums:
-#     final.append(num * myFactor)
-# print(final)
+myFactor = 12
+nums = [-4,-5,100,4,-4,3,6,0]
+final = []
+for num in nums:
+    final.append(num * myFactor)
+print(final)
 
 
 #8
-# list1 = [1,2,4]
-# list2 = [3,3,6]
-# final = []
-# for i in range(len(list1)):
-#     final.append(list1[i]*list2[i])
-# print(final)
+list1 = [1,2,4]
+list2 = [3,3,6]
+final = []
+for i in range(len(list1)):
+    final.append(list1[i]*list2[i])
+print(final)
 
 #9
 
-# list1 = [[1, 3], [2, 4]]
-# list2 = [[5, 2], [1, 0]]
-# final = [[0, 0], [0, 0]]
-# for i in range(2):
-#     for j in range(2):
-#         newnum = list1[i][j] + list2[i][j]
-#         final[i][j] = newnum
-# print(final)
+list1 = [[1, 3], [2, 4]]
+list2 = [[5, 2], [1, 0]]
+final = [[0, 0], [0, 0]]
+for i in range(2):
+    for j in range(2):
+        newnum = list1[i][j] + list2[i][j]
+        final[i][j] = newnum
+print(final)
 
 
 #10 --- ???
 
-# list1 = [[1, 3], [2, 4]]
-# list2 = [[5, 2], [1, 0]]
-# final = [[0, 0], [0, 0]]
-# for i in range(2):
-#     for j in range(2):
-#         newnum = list1[i][j] + list2[i][j]
-#         final[i][j] = newnum
-# print(final)
+list1 = [[1, 3], [2, 4]]
+list2 = [[5, 2], [1, 0]]
+final = [[0, 0], [0, 0]]
+for i in range(2):
+    for j in range(2):
+        newnum = list1[i][j] + list2[i][j]
+        final[i][j] = newnum
+print(final)
 
 #11
 
-# nums = [-4,-5,100,4,100,6,-4,3,6,0]
-# final = []
-# for num in nums:
-#     if num not in final:
-#         final.append(num)
-# print(final)
+nums = [-4,-5,100,4,100,6,-4,3,6,0]
+final = []
+for num in nums:
+    if num not in final:
+        final.append(num)
+print(final)
 
-#bonus
+bonus
--- ex2_loops.py
+++ ex2_loops.py
@@ -1,94 +1,102 @@
-# I am assuming all user-generated input is within the scope of the question. 
+# # I am assuming all user-generated input is within the scope of the question. 
 
-#1
-for i in range(1, 11):
-    print(i)
+# #1
+# for i in range(1, 11):
+#     print(i)
 
-#2
-start = int(input("Which number shall we start on? > "))
-end = int(input("Which number shall we end on? > "))
+# #2
+# start = int(input("Which number shall we start on? > "))
+# end = int(input("Which number shall we end on? > "))
 
-for i in range(start, end+1):
-    print(i)
+# for i in range(start, end+1):
+#     print(i)
 
-#3
-for i in range(1, 10, 2):
-    print(i)
+# #3
+# for i in range(1, 10, 2):
+#     print(i)
 
-### OR ###
+# ### OR ###
 
-for i in range(1, 10):
-    if i % 2 != 0:
-        print(i)
+# for i in range(1, 10):
+#     if i % 2 != 0:
+#         print(i)
 
-#4
-for i in range(5):
-    print("*" * 5)
+# #4
+# for i in range(5):
+#     print("*" * 5)
 
-#5
-num = int(input("How big is the square? > "))
-for i in range(num):
-    print("*" * num)
+# #5
+# num = int(input("How big is the square? > "))
+# for i in range(num):
+#     print("*" * num)
 
-#6
-height = int(input("How tall should the box be? > "))
-width = int(input("How wide should the box be? > "))
-for i in range(height):
-    if i == 0 or i == (height - 1):
-        print("*" * width)
-    else: 
-        print("*" + (" " * (width - 2) + "*"))
+# #6
+# height = int(input("How tall should the box be? > "))
+# width = int(input("How wide should the box be? > "))
+# for i in range(height):
+#     if i == 0 or i == (height - 1):
+#         print("*" * width)
+#     else: 
+#         print("*" + (" " * (width - 2) + "*"))
 
 
-#7 AND 8
-while True:
-    height = int(input("How high shall the tree be? > "))
-    if height < 0:
-        print("Sorry, we need a positive number.")
-    if height % 2 == 0:
-        print("Sorry, we need an odd number")
-    else:
-        break
+# #7 
 
-rows = []
-i = 0
-while 2*i < height:
-    newrow = " "*i + "*"*(height - i*2) + " "*i
-    rows.append(newrow)
+height = 4
+row = []
+while (2*i) < height:
+    newrow = " "*i
     i += 1
 
-index = len(rows)
-for x in range(len(rows)-1, 0, -1):
-    print(rows[x])
+# # 8
+# while True:
+#     height = int(input("How high shall the tree be? > "))
+#     if height < 0:
+#         print("Sorry, we need a positive number.")
+#     if height % 2 == 0:
+#         print("Sorry, we need an odd number")
+#     else:
+#         break
 
-#9
-for i in range(1,11):
-    for j in range(1, 11):
-        product = i * j
-        print(f"{i} X {j} = {product} ")
+# rows = []
+# i = 0
+# while 2*i < height:
+#     newrow = " "*i + "*"*(height - i*2) + " "*i
+#     rows.append(newrow)
+#     i += 1
 
-#bonus1
-myMessage = input("What's your message? > ")
-length = len(myMessage)
-border = "*" * (length + 4)
-print(border)
-print(f"* {myMessage} *")
-print(border)
+# index = len(rows)
+# for x in range(len(rows)-1, 0, -1):
+#     print(rows[x])
 
-#bonus 2
-triNums = []
-i = 1
-while len(triNums) < 100:
-    newNum = int(i*(i+1)/2)
-    triNums.append(newNum)
-    i+= 1
-print(triNums)
+# #9
+# for i in range(1,11):
+#     for j in range(1, 11):
+#         product = i * j
+#         print(f"{i} X {j} = {product} ")
+
+# #bonus1
+# myMessage = input("What's your message? > ")
+# length = len(myMessage)
+# border = "*" * (length + 4)
+# print(border)
+# print(f"* {myMessage} *")
+# print(border)
+
+# #bonus 2
+# triNums = []
+# i = 1
+# while len(triNums) < 100:
+#     newNum = int(i*(i+1)/2)
+#     triNums.append(newNum)
+#     i+= 1
+# print(triNums)
 
 
-#bonus 3
-myNum = int(input("Pick a number > "))
-factors = []
-for i in range(1, myNum):
-    if myNum % i == 0:
-        factors.append(i)
-print(factors)
+# #bonus 3
+# myNum = int(input("Pick a number > "))
+# factors = []
+# for i in range(1, myNum):
+#     if myNum % i == 0:
+#         factors.append(i)
+# print(factors)

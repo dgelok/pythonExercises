3c72238dc926413bcc2cdcb010384f6d4dc07d0b
Dan Gelok <dgelok@gmail.com>
2020-09-26 19:35:42 -0500

    added new algo

---
+++ algorithms/factorialDigitSum.py
@@ -0,0 +1,19 @@
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = str(factorial(100))
+total = 0
+for i in range(len(num)):
+    total += int(num[i])
+
+print(total)

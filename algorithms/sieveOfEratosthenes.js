5808e645b802e1fd42a4fd5b3f5733e4d3bd0377
Dan Gelok <dgelok@gmail.com>
2020-10-21 10:20:07 -0500

    sieve of eratosthenes

---
+++ algorithms/sieveOfEratosthenes.js
@@ -0,0 +1,35 @@
+// for a number n, return all the prime numbers up to n. 
+
+const sieve = (n) =>{
+    let myNums = []
+    for (let i = 1; i <= n+1; i++) {
+        myNums.push(true)
+    }
+    console.log(myNums.length)
+    
+    // we now have myNums - an array of length n - where all the entries are true
+    // now we go through and remove all multiples
+
+    let p = 2;
+    while (p*p < n) {
+        if (myNums[p]) {
+            for (let y = (p*2); y < n; y += p) {
+                myNums[y] = false
+            }
+        }
+        p++
+    }
+
+    for (let l = 0; l < n; l++) {
+        if (myNums[l] === true) {
+            console.log(l)
+        }
+    }
+
+    
+    // at this point, myNums has true for primes, and false for all of the multiples of those primes
+
+}
+
+
+sieve(100)
